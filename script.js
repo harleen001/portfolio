f452c2bb5813e5c4553067728762cf059f39fb7a
Harleen Singh <harleen.johal31@gmail.com>
2023-10-22 16:37:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    slidePerView: 1,
+    spaceBetween: 0,
+    mousewheel: true,
+    pagination: {
+        el:'.swiper-pagination',
+        type: 'progressbar',
+    }
+}) 
+
